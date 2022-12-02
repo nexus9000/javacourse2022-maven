55e8e0515474285642a5a6b8d0515d07ea43edec
User <java.user.tester@gmail.com>
2022-12-02 20:28:45 +0200

    add sql logic

--- sql-module/src/main/java/edu/itstep/sql/SqlOps.java
+++ sql-module/src/main/java/edu/itstep/sql/SqlOps.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 
 public class SqlOps {
-    private static final String URL = "jdbc:sqlite:C:/sqlite/test.db";
+    private static final String URL = "jdbc:sqlite:database/test.db";
     private static Connection conn;
     private Statement st;
     private static PreparedStatement ps;
